5c3acd98588d04dfc62152ae76b9f1a07fd83f2c
lework <lework@yeah.net>
2020-02-28 11:56:39 +0800

    add pihole

---
+++ pihole/adblock.sh
@@ -0,0 +1,4 @@
+curl -s -L https://easylist-downloads.adblockplus.org/easylistchina+easylist.txt https://easylist-downloads.adblockplus.org/malwaredomains_full.txt https://easylist-downloads.adblockplus.org/fanboy-social.txt > adblock.unsorted
+sort -u adblock.unsorted | grep ^\|\|.*\^$ | grep -v \/ > adblock.sorted
+sed 's/[\|^]//g' < adblock.sorted > adblock.hosts
+rm adblock.unsorted adblock.sorted
